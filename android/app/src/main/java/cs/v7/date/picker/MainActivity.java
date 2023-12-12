2c1274ddcd5c0d310049faff93087d39fbe551d6
damian.ionic <damian@ionic.io>
2023-12-12 07:15:57 -0800

    Initial

---
+++ android/app/src/main/java/cs/v7/date/picker/MainActivity.java
@@ -0,0 +1,5 @@
+package cs.v7.date.picker;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cs.v7.date.picker',
+  appName: 'cs-v7-date-picker',
+  webDir: 'www/browser',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
